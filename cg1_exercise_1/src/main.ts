5f328b495906672b27e3b9043866bc015d386f47
SirArchy <wnbemail123@googlemail.com>
2022-11-09 21:44:01 +0100

    first task complete

--- cg1_exercise_1/src/main.ts
+++ cg1_exercise_1/src/main.ts
@@ -53,26 +53,25 @@ function main(){
 
     // create objects & add to scene
     const robotBody = new THREE.Object3D();
+    const arm1Joint = new THREE.Object3D();
+    const arm2Joint = new THREE.Object3D();
+    const foot1Joint = new THREE.Object3D();
+    const foot2Joint = new THREE.Object3D();
+    const headJoint = new THREE.Object3D();
     const foot1 = new THREE.Object3D();
-    const foot2 = new THREE.Object3D();
-    scene.add(robotBody, foot1, foot2);
+    const foot2 = new THREE.Object3D();    
 
     // create geometries for bodies
-    /*
-    const robotBodyGeometry = new THREE.BoxGeometry(1, 2.5, 1);
-    const headGeometry = new THREE.BoxGeometry(.5, 1.5, .5);
-    const armGeometry = new THREE.BoxGeometry(.5, 3.5, 1);
-    const footGeometry = new THREE.BoxGeometry(1, 3, 1);
-    const toeGeometry = new THREE.BoxGeometry(.5, .5, 1);
-    */
-
-    const armGeometry = new THREE.BoxGeometry(.5, 3.5, 1);
+    const robotBodyGeometry = new THREE.BoxGeometry(2.5, 3, 1);
+    const headGeometry = new THREE.BoxGeometry(1, 1, .75);
+    const armGeometry = new THREE.BoxGeometry(.5, 2, .5);
+    const footGeometry = new THREE.BoxGeometry(.5, 3, .5);
+    const toeGeometry = new THREE.BoxGeometry(.5, .5, .5);
 
     // create material for bodies
     const boxMaterial = new THREE.MeshPhongMaterial({emissive: 0x111111});
 
-    // create meshes & add to scene
-    /*
+    // create meshes
     const robotBodyMesh = new THREE.Mesh(robotBodyGeometry, boxMaterial);
     const headMesh = new THREE.Mesh(headGeometry, boxMaterial)
     const arm1Mesh = new THREE.Mesh(armGeometry, boxMaterial);    
@@ -81,47 +80,92 @@ function main(){
     const foot2Mesh = new THREE.Mesh(footGeometry, boxMaterial);
     const toe1Mesh = new THREE.Mesh(toeGeometry, boxMaterial);
     const toe2Mesh = new THREE.Mesh(toeGeometry, boxMaterial);
+
+    // create scene graph
+    scene.add(robotBody);
+    robotBody.add(arm1Joint, arm2Joint, foot1Joint, foot2Joint, headJoint);
+    foot1Joint.add(foot1);
+    foot2Joint.add(foot2);
     foot1.add(foot1Mesh, toe1Mesh);
     foot2.add(foot2Mesh, toe2Mesh);
-    robotBody.add(robotBodyMesh, headMesh, arm1Mesh, arm2Mesh, foot1, foot2);
-    */
-
-    const arm1Mesh = new THREE.Mesh(armGeometry, boxMaterial); 
-    const arm2Mesh = new THREE.Mesh(armGeometry, boxMaterial); 
-    robotBody.add(arm1Mesh,arm2Mesh); 
+    arm1Joint.add(arm1Mesh);
+    arm2Joint.add(arm2Mesh);
+    headJoint.add(headMesh);
+    robotBody.add(robotBodyMesh);
 
     // create 4x4 matrices for transformations
-    arm1Mesh.matrix.set(1, 2, 3, 4,
-      1, 2, 3, 4,
-      1, 2, 3, 4,
-      1, 2, 3, 4 )
-    arm1Mesh.updateMatrix();   
+    arm1Mesh.matrixWorld.set(1, 0, 0, 2,
+      0, 1, 0, 0.5,
+      0, 0, 1, 0,
+      0, 0, 0, 1,)
+      arm1Mesh.updateMatrixWorld(); 
+
+    arm2Mesh.matrixWorld.set(1, 0, 0, -2,
+      0, 1, 0, 0.5,
+      0, 0, 1, 0,
+      0, 0, 0, 1,)
+      arm2Mesh.updateMatrixWorld(); 
+
+    foot1Mesh.matrixWorld.set(1, 0, 0, 0.5,
+      0, 1, 0, -3.5,
+      0, 0, 1, 0,
+      0, 0, 0, 1,)
+      foot1Mesh.updateMatrixWorld(); 
+
+    foot2Mesh.matrixWorld.set(1, 0, 0, -0.5,
+      0, 1, 0, -3.5,
+      0, 0, 1, 0,
+      0, 0, 0, 1,)
+      foot2Mesh.updateMatrixWorld(); 
+
+    toe1Mesh.matrixWorld.set(1, 0, 0, 0.5,
+      0, 1, 0, -4.75,
+      0, 0, 1, .5,
+      0, 0, 0, 1,)
+      toe1Mesh.updateMatrixWorld(); 
+
+    toe2Mesh.matrixWorld.set(1, 0, 0, -0.5,
+      0, 1, 0, -4.75,
+      0, 0, 1, .5,
+      0, 0, 0, 1,)
+      toe2Mesh.updateMatrixWorld(); 
+
+    headMesh.matrixWorld.set(1, 0, 0, 0,
+      0, 1, 0, 2.5,
+      0, 0, 1, 0,
+      0, 0, 0, 1,)
+      headMesh.updateMatrixWorld(); 
+
+    arm1Joint.matrixWorld.set(1, 0, 0, 1,
+      0, 1, 0, 0.5,
+      0, 0, 1, 0,
+      0, 0, 0, 1,)
+      arm1Joint.updateMatrixWorld(); 
+
+    arm2Joint.matrixWorld.set(1, 0, 0, -1,
+      0, 1, 0, 0.5,
+      0, 0, 1, 0,
+      0, 0, 0, 1,)
+      arm2Joint.updateMatrixWorld(); 
+
+    foot1Joint.matrixWorld.set(1, 0, 0, 0.5,
+      0, 1, 0, -1,
+      0, 0, 1, 0,
+      0, 0, 0, 1,)
+      foot1Joint.updateMatrixWorld(); 
+
+    foot2Joint.matrixWorld.set(1, 0, 0, -0.5,
+      0, 1, 0, -1,
+      0, 0, 1, 0,
+      0, 0, 0, 1,)
+      foot2Joint.updateMatrixWorld(); 
+
+    headJoint.matrixWorld.set(1, 0, 0, 0,
+      0, 1, 0, 1.5,
+      0, 0, 1, 0,
+      0, 0, 0, 1,)
+      headJoint.updateMatrixWorld(); 
       
-    
-    const arm2PosSetup = new THREE.Matrix4();
-    arm2PosSetup.set( 11, 12, 13, 14,
-       21, 22, 23, 24,
-       31, 32, 33, 34,
-       41, 42, 43, 44 );
-
-    const foot1PosSetup = new THREE.Matrix4();
-    foot1PosSetup.set( 11, 12, 13, 14,
-       21, 22, 23, 24,
-       31, 32, 33, 34,
-       41, 42, 43, 44 );
-
-    const foot2PosSetup = new THREE.Matrix4();
-    foot2PosSetup.set( 11, 12, 13, 14,
-       21, 22, 23, 24,
-       31, 32, 33, 34,
-       41, 42, 43, 44 );
-   
-    const headPosSetup = new THREE.Matrix4();
-    headPosSetup.set( 11, 12, 13, 14,
-       21, 22, 23, 24,
-       31, 32, 33, 34,
-       41, 42, 43, 44 );
-   
 
     // start the animation loop (async)
     var wid = new RenderWidget(rendererDiv, renderer, camera, scene, controls);
